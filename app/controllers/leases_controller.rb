09216f745b6017cde12494c4e0d72b1f1b1a365a
margaret manyeki <maggiemanyeki6@gmail.com>
2023-03-22 16:56:46 +0300

    updated & added sln

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,5 +1,9 @@
 class LeasesController < ApplicationController
 
+    def index
+        render json: Lease.all
+    end
+
     def create 
         lease = Lease.create(lease_params)
         render json: lease, status: :created
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :tenants
-  resources :leases
+  resources :leases, only: [:index, :create, :destroy]
   resources :apartments
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
